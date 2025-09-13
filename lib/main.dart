d42e0dd69c7598f9d2ae1cc52622ac5c0768e2d2
Louis <Lou.ikwuegbu@gmail.com>
2025-09-13 21:50:41 +0100

    added stateful widget and image randomization for dice roll

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
+
 
 void main() {
   return runApp(
@@ -15,8 +17,23 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  
   @override
+ _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumber =  5; //Random().nextInt(6) + 1;
+  int rightDice = 5;
+
+  void changeDiceFace() {
+    setState(() {
+      leftDiceNumber = Random().nextInt(6) + 1;
+      rightDice = Random().nextInt(6) +1;
+      });
+  }
+ @override
   Widget build(BuildContext context) {
     return Center(child: 
       Row(
@@ -25,9 +42,9 @@ class DicePage extends StatelessWidget {
           Expanded(
             child:TextButton(
               onPressed: () {
-                print('left button engaged');
+                changeDiceFace();
               },
-              child: Image.asset('images/dice1.png'),
+              child: Image.asset('images/dice$leftDiceNumber.png'),
             ),
           ),
   
@@ -38,12 +55,15 @@ class DicePage extends StatelessWidget {
           Expanded(
             child: TextButton(
               onPressed: () {
-                print('Right button engaged');
+                changeDiceFace();
               },
-              child: Image.asset('images/dice1.png')),
+              child: Image.asset('images/dice$rightDice.png')),
           ),
         ],
       ),
     );
   }
 }
+
+
+
